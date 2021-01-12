9eed9ce4e2f59ae1a19e2a56bf3f3cf752d48137
phihd <phictin@gmail.com>
2021-01-12 04:05:27 +0200

    refactor files

--- Raw Code/Statistic.scala
+++
@@ -1,5 +0,0 @@
-package o1.adventure
-
-class Statistic (var HP : Int, var Attack: Int, var Defense: Int, var Speed : Int, var Critical : Int) {
-  override def toString = HP + " health " + ", Attack: " + this.Attack + ", Defense: " + this.Defense + ", Speed: " + Speed + ", Critical rate: " + Critical + "%"
-}
